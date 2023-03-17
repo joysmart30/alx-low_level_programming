9251039843c8f91096ce7a0113c8123f1de2f181
Joy Emeh <joysmart30@gmail.com>
2023-03-16 18:09:11 -0700

    Name putting

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - the main function
+ *
+ * Return: always 0
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
