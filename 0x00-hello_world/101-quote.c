634545e7331cb7a604d3bd222a0cd2ed903c4fe7
Joy Emeh <joysmart30@gmail.com>
2023-03-17 08:55:33 -0700

    Quote hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+*main - Entry point
+* explain what your code does
+* Return: 1
+*/
+int main(void)
+{
+	write(2, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	return (1);
+}
