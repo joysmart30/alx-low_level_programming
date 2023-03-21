548905323d3a170003ab17823f710df3a5830118
Joy Emeh <joysmart30@gmail.com>
2023-03-21 06:48:47 -0700

    the headers

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,36 +1,16 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
- * main - Print _putchar
- * Return: Always 0
- * Originally, the header file i used in main.h was
- * int _putchar (char *)
- * this is because i wanted my output to be
- * a string of _putchar and i wont type so much
- * and so my original description was
- *
- * char *x;
- *
- * x = "_putchar";
- * printf("%s\n", x);
- *
- * and it worked
- * but on getting to Task 1, i realised
- * i still need to use the function _putchar and
- * it can only print out a char
- * so my code had to be modified to this...
- *
- * by HilarySmartt
+ * _putchar - writes the character c to stdout
+ * @c: The character to rint
+ * Return: On sucess 1.
+ * On error, -1 is returned, and error is set appropriately.
  */
-int main(void)
+
+int _putchar(char c)
+
 {
-putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+
+	return (write(1. &c, 1));
+
 }
