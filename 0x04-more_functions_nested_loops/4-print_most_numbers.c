a7394231f3a8cb3e728f39b34f5c36f2f68ffd32
Joy Emeh <joysmart30@gmail.com>
2023-03-25 03:45:12 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * more_numbers - nmbr fct
+ * print_most_numbers - checks for checks for a digit (0 through 9).
  *
- * Return: no return
- */
+ * Return: Always 0.
+*/
 
-void more_numbers(void)
+void print_most_numbers(void)
 {
+ int c;
 
-int i, j;
-	for (i = 1; i <= 10; i++)
-	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-			_putchar('1');
-			_putchar(j % 10 + '0');
-		}
-			_putchar('\n');
-		}
+ for (c = 48; c < 58; c++)
+ {
+ if (c != 50)
+ {
+ if (c != 52)
+ {
+ _putchar(c);
+ }
+ }
+ }
+ _putchar('\n');
 }
