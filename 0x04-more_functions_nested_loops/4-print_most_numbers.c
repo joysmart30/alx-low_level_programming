0f254cf019a786b8ed0ca4c663be2762dd269dc0
Joy Emeh <joysmart30@gmail.com>
2023-03-26 15:16:33 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
-*/
-
+ * print_most_numbers - prints numbers
+ * Return:0
+ */
 void print_most_numbers(void)
 {
- int c;
+char n = 0;
 
- for (c = 48; c < 58; c++)
- {
- if (c != 50)
- {
- if (c != 52)
- {
- _putchar(c);
- }
- }
- }
- _putchar('\n');
+while (n <= 9)
+{
+if (n != 2 && n != 4)
+{
+_putchar('0' + n);
+}
+
+n++;
+}
+_putchar('\n');
 }
