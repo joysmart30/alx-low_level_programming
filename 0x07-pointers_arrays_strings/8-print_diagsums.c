4cec9be3960c60579ff0284aa0abc5da90889564
Joy Emeh <joysmart30@gmail.com>
2023-04-15 11:42:16 -0700

    Fnx that prints the sum of the 2 diagonals of a square matrix of int.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,22 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix
- * @a: pointer to start of matrix
- * @size: width of matrix column
- *
- * Return: void
+  * print_diagsums - the function name
+  * @a: parameter of type int .
+  * @size: parameter of type int .
+  * Return: void .
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i;
+	int sum1, sum2;
 
-	for (i = 0; i < (size * size; i++)
+	sum1 = 0;
+	sum2 = 0;
+	i = 0;
+	while (i < size)
 	{
-		if (i % (size + 1) == 0)
-			sum1 += *(a + i);
-		if (i % (size - 1) == 0 && i < size * size - 1)
-			sum2 += *(a + i);
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+		i++;
 	}
-	printf("%d, %d\n", sum1, sum2);
+	printf("%i, %i\n", sum1, sum2);
 }
