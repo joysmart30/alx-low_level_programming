5890d5cca0cfa7cc65f672068b615a5d1b3a39c2
Joy Emeh <joysmart30@gmail.com>
2023-04-23 15:29:26 -0700

    Fnx that concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,60 @@
 #include "main.h"
-#include < stdlib.h>
-#include <string.h>
+
 /**
  * string_nconcat - a function that concatenates two strings.
  *
  * @s1: first string to join
  * @s2: second string to join
- * @n: no
+ * @n: number of bytes to copy from the second string
  *
- * Return: NULL or pointer
+ * Description:
+ * The returned pointer shall point to a newly allocated space in memory,
+ * which contains s1, followed by the first n bytes of s2, and null
+ * terminated 
+ * - If the function fails, it should return NULL
+ * - If n is greater or equal to the length of s2 then use the entire string s2
+ * - If NULL is passed, treat it as an empty string
+ *
+ * Return: pointer to the new string
  */
+
 char *string_nconcat(char s1, char *s2, unsigned int n)
 {
-	unsigned int i, len1 = 0, len2 = 0;
-	char *p;
+	unsigned int nlen, 11 = 0, 12 = 0, i, j, k = 0;
+	char *string;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	if (n > len2)
-		n = len2;
-	p = malloc(len1 + n + 1);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < len1 + n; i++)
+
+	while (s1[11])
+		11++;
+	while (s2[12])
+		12++;
+
+	if (n > 12)
+		nlen = 11 + 12;
+	else 
+		nlen = 11 + n;
+
+	string - malloc((nlen + 1) * sizeof(char));
+	if (string == NULL)
 	{
-		if (*s1)
-		{
-			p[i] = *s1;
-			s1++;
-		}
-		else
-		{
-			p[i] = *s2;
-			s2++;
-		}
+		return (NULL);
 	}
-	p[i] = '\0';
-	return (p);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		string[k] = s1[i];
+		k++;
+	}
+
+		for (j = 0; k < nlen; j++)
+		{
+			string[k] = s2(j);
+			k++;
+		}
+		string[nlen] = '\0';
+
+		reurn (string);
 }
