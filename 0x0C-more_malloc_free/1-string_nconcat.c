66bdbd4d76f2be06f80fcb433e78df8d8bfe6c95
Joy Emeh <joysmart30@gmail.com>
2023-04-23 15:03:02 -0700

    Fnx that concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,48 +4,41 @@
 /**
  * string_nconcat - a function that concatenates two strings.
  *
- * @s1: first char
- * @s2: second char
- * @n: unsigned int
+ * @s1: first string to join
+ * @s2: second string to join
+ * @n: no
  *
- * Return: If the function fails, it should return NULL
+ * Return: NULL or pointer
  */
 char *string_nconcat(char s1, char *s2, unsigned int n)
 {
-	unsigned int x, y, z;
-	char *s;
+	unsigned int i, len1 = 0, len2 = 0;
+	char *p;
 
 	if (s1 == NULL)
-	{
-		x = 0;
-	}
-	else
-	{
-
-		for (x = 0; s1[x]; ++x)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-
-		y = 0;
-
-	}
-	else
-	{
-
-		for (y = 0; s2[y]; ++y)
-			;
-	}
-	if (y > n)
-		y = n;
-	s = malloc(sizeof(char) * (x + y + 1));
-	if (s == NULL)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n > len2)
+		n = len2;
+	p = malloc(len1 + n + 1);
+	if (p == NULL)
 		return (NULL);
-	for (z = 0; z < x; z++)
-		s[z] = s1[z];
-	for (z = 0; z < y; z++)
-		s[z + x] = s2[z];
-	s[x + y] = '\0';
-	return (s);
+	for (i = 0; i < len1 + n; i++)
+	{
+		if (*s1)
+		{
+			p[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			p[i] = *s2;
+			s2++;
+		}
+	}
+	p[i] = '\0';
+	return (p);
 }
