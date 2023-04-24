e83acb70af05d42a38001ce76a62c8ecc8229319
Joy Emeh <joysmart30@gmail.com>
2023-04-24 10:53:39 -0700

    Fnx that concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,37 +19,44 @@
  * Return: pointer to the new string
  */
 
-char *string_nconcat(char s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int nlen, l1 = 0, l2 = 0, i, j, k = 0;
+	char *string;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
+	while (s1[l1])
+		l1++;
+	while (s2[l2])
+		l2++;
+
+	if (n > l2)
+		nlen = l1 + l2;
 	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		nlen = l1 + n;
 
-	if (!s)
-		return (NULL);
-
-	while (i < len1)
+	string = malloc((nlen + 1) * sizeof(char));
+	if (string == NULL)
 	{
-		s[i] = s1[i];
-		i++;
+		return (NULL);
 	}
 
-	while (n < len2 && i < (len1 + n))
-			s[i++] = s2[j++];
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		string[k] = s1[i];
+		k++;
+	}
 
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
+	for (j = 0; k < nlen; j++)
+	{
+		string[k] = s2[j];
+		k++;
+	}
+	string[nlen] = '\0';
 
-	s[i] = '\0';
-
-	return (s);
+	return (string);
 }
