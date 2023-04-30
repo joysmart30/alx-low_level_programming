1351af31edf808abd99aebaa98ddda08d350db8b
Joy Emeh <joysmart30@gmail.com>
2023-04-30 11:01:43 -0700

    Fnx that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,28 @@
 #include "lists.h"
-
+#include <string.h>
 /**
- * add_node - Adds a new node at the beginning
- *            of a list.
- * @head: A pointer to the head of the list.
- * @str: is a chracter array.
- *
- * Return: a pointer to the first node.
+ * add_node - adds a  node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int i = 0;
-	char *duplicate;
+	list_t *new_node;
+	size_t char_num;
 
-
-	list_t *new_node = malloc(sizeof(list_t));
-
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
-	while (str[i] != '\0')
-	{
-		i++;
-	}
+	new_node->str = strdup(str);
 
-	duplicate = strdup(str);
+	for (char_num = 0; str[char_num]; char_num++)
+		;
 
-
-	new_node->str = duplicate;
-	new_node->len = i;
+	new_node->len = char_num;
 	new_node->next = *head;
-
 	*head = new_node;
 
 	return (*head);
