400416082a1e5dbc19a1a22d31ee67aeef88e226
Joy Emeh <joysmart30@gmail.com>
2023-05-01 15:11:58 -0700

    Fnx that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,24 +4,25 @@
 /**
  * print_list - Prints all the elements in a list
  * @h: name of the list
+ *
  * Return: the  number of nodes.
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t nodes = 0;
 
 	while (h)
-	{
+
 		if (h->str == NULL)
-		{
+
 			printf("[0] (nil)\n");
 
 		else
 
 			printf("[%d] %s\n", h->len, h->str);
-		}
-		count++;
+
+		nodes++;
 		h = h->next;
 	}
-	return (count);
+	return (nodes);
 }
