6c39309aa9844bd34c89c49210d31303cb730abc
Joy Emeh <joysmart30@gmail.com>
2023-05-01 17:11:36 -0700

    Fxn that prints ... before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,11 @@
 
 /**
  * bmain - function executed before main
- * Return: no return
+ * Return: no return.
  */
 
 void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet! you must allow");
+	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
