bf811f27e501c30bf523b55e2df3bf45581a80a2
Joy Emeh <joysmart30@gmail.com>
2023-05-21 15:23:20 -0700

    Write a Unix command line interpreter

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file
- * @filename: A pointer to the name of the file to create
+ * create_file - Create the file.
+ * @filename: A pointer to the name of the file to be created.
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *	Otherwise - 1.
+ *         Otherwise - 1.
  */
 
 int create_file(const char *filename, char *text_content)
@@ -22,7 +22,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | 0_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
